8afdafcb327365029a554d80cdfb00b8c669ca85
Mariola Pasko <paskomariola@gmail.com>
2019-12-04 16:00:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,8 @@ array.each_with_index {|element,index|
 puts "#{index +1} #{element}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+planeteer_calls.map {|element| "#{element}!".capitalize}
 end
 
 def long_planeteer_calls# code an argument here
